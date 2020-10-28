5aea727a248ada5ca129e6cca22736447c7bd39c
jake-g <omonoid@gmail.com>
2020-10-28 10:34:26 -0700

    update brew log folder

--- run_brew_logger.py
+++ run_brew_logger.py
@@ -35,15 +35,15 @@ LOG_CONF = {
     # Extra stdout logging verbosity for debug purposes
     "debug": True,
     # Name of local .tsv file for logging session.
-    "local_logfile": os.path.join(cwd, "homebrew-logs/log_%d.tsv" % time.time()),
+    "local_logfile": os.path.join(cwd, "brew-logs/log_%d.tsv" % time.time()),
     # Log data every LOG_PERIOD seconds.
     "log_period": 60 * 5,
     # Name of local .tsv file to backup data to.
-    "local_backup": os.path.join(cwd, "homebrew-logs/gsheet_bkp.tsv"),
+    "local_backup": os.path.join(cwd, "brew-logs/gsheet_bkp.tsv"),
     # Backup data locally every LOG_PERIOD seconds.
     "backup_period": 60 * 20,
     # Name of Google Spreadsheet to write to.
-    "gsheet_name": "homebrew-log",
+    "gsheet_name": "brew-log",
     # Maximum number of decimal places for entry.
     "max_precision": 4,
     # Authentication json for GDrive api.
